240ac2e85c371f8bb00bb7064f4b84d3d6c19e9a
F7Pz <55998951+F7Pz@users.noreply.github.com>
2019-09-30 21:43:53 +0300

    Add files via upload

---
+++ ath.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link href="css/styles1.css" rel ="stylesheet">
+	<title>ALATHEM </title>
+</head>
+
+<body>
+<!-- code shopping -->
+
+<section id = "azooz" style="max-width: 95.2428571429rem;">
+	<div class="card" style=" float: right; padding: 5px;" >
+	  <img src="images/apple.png" alt="Denim Jeans"  style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+
+	<div class="card2" style=" float: right; padding: 5px;" >
+	  <img src="images/apple.png" alt="Denim Jeans"  style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+
+	<div class="card3" style=" float: right; padding: 5px;">
+	  <img src="images/apple.png" alt="Denim Jeans" style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+	<div class="card" style=" float: right; padding: 5px;" >
+	  <img src="images/apple.png" alt="Denim Jeans"  style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+
+	<div class="card2" style=" float: right; padding: 5px;" >
+	  <img src="images/apple.png" alt="Denim Jeans"  style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+
+	<div class="card3" style=" float: right; padding: 5px;">
+	  <img src="images/apple.png" alt="Denim Jeans" style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+	<div class="card" style=" float: right; padding: 5px;" >
+	  <img src="images/apple.png" alt="Denim Jeans"  style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+
+	<div class="card2" style=" float: right; padding: 5px;" >
+	  <img src="images/apple.png" alt="Denim Jeans"  style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+
+	<div class="card3" style=" float: right; padding: 5px;">
+	  <img src="images/apple.png" alt="Denim Jeans" style="width:100%">
+	  <h1>apple </h1>
+	  <p class="price">$19.99</p>
+	  <p>Some text about the jeans..</p>
+	  <p><button>Add to Cart</button></p>
+	</div>
+	<nav>
+  <div class="container">
+    <ul class="navbar-left">
+      <li><a href="#">Home</a></li>
+      <li><a href="#about">About</a></li>
+    </ul> <!--end navbar-left -->
+
+    <ul class="navbar-right">
+      <li><a href="#" id="cart"><i class="fa fa-shopping-cart"></i> Cart <span class="badge">3</span></a></li>
+    </ul> <!--end navbar-right -->
+  </div> <!--end container -->
+</nav>
+
+
+<div class="container">
+  <div class="shopping-cart">
+    <div class="shopping-cart-header">
+      <i class="fa fa-shopping-cart cart-icon"></i><span class="badge">3</span>
+      <div class="shopping-cart-total">
+        <span class="lighter-text">Total:</span>
+        <span class="main-color-text">$2,229.97</span>
+      </div>
+    </div> <!--end shopping-cart-header -->
+
+    <ul class="shopping-cart-items">
+      <li class="clearfix">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/cart-item1.jpg" alt="item1" />
+        <span class="item-name">Sony DSC-RX100M III</span>
+        <span class="item-price">$849.99</span>
+        <span class="item-quantity">Quantity: 01</span>
+      </li>
+
+      <li class="clearfix">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/cart-item2.jpg" alt="item1" />
+        <span class="item-name">KS Automatic Mechanic...</span>
+        <span class="item-price">$1,249.99</span>
+        <span class="item-quantity">Quantity: 01</span>
+      </li>
+
+      <li class="clearfix">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/cart-item3.jpg" alt="item1" />
+        <span class="item-name">Kindle, 6" Glare-Free To...</span>
+        <span class="item-price">$129.99</span>
+        <span class="item-quantity">Quantity: 01</span>
+      </li>
+    </ul>
+
+    <a href="#" class="button">Checkout</a>
+  </div> <!--end shopping-cart -->
+</div> <!--end container -->
+
+</section>
+
+<!-- end code shopping -->
+</body>
+</html>
